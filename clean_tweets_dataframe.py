55df668a1f108767c940d456dc5a7cb4ad2209d3
Stella Kibika <stellakibika@gmail.com>
2021-06-24 21:44:50 +0300

    extract dataframe from json and clean

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,64 +1,70 @@
 import nltk
 import numpy as np
 import pandas as pd
+from extract_dataframe import tweet_df
+
+
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
     """
-    def __init__(self, df:pd.DataFrame):
+
+    def __init__(self, df: pd.DataFrame):
         self.df = df
         print('Automation in Action...!!!')
-        
-    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
+
+    def drop_unwanted_column(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         remove rows that has column names. This error originated from
-        the data collection stage.  
+        the data collection stage.
         """
-        unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
-        df.drop(unwanted_rows , inplace=True)
-        df = df[df['polarity'] != 'polarity']
-        
+        df = df[df['retweet_count'] == 'retweet_count']
+        df.reset_index()
+
         return df
-    def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
+
+    def drop_duplicate(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         drop duplicate rows
         """
-        df=df.drop_duplicates(subset=None, keep='first', inplace = True)
+        df = df.iloc[df.astype(str).drop_duplicates().index]
+        df.reset_index()
 
-        
         return df
-    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
+
+    def convert_to_datetime(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         convert column to datetime
         """
         df['created_at'] = pd.to_datetime(df['created_at']).dt.date
 
-        df = df[df['created_at'] >= '2020-12-31' ]
-        
+        df = df[df['created_at'] >= pd.to_datetime("2020-12-31").date()]
+        df.reset_index()
         return df
-    
-    def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
+
+    def convert_to_numbers(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
         df['polarity'] = pd.to_numeric(df['polarity'])
-        df['quote_count'] = pd.to_numeric(df['quote_count'])
-        df['reply_count'] = pd.to_numeric(df['reply_count'])
+        df['subjectivity'] = pd.to_numeric(df['subjectivity'])
+        # df['quote_count'] = pd.to_numeric(df['quote_count'])
+        # df['reply_count'] = pd.to_numeric(df['reply_count'])
         df['retweet_count'] = pd.to_numeric(df['retweet_count'])
         df['favorite_count'] = pd.to_numeric(df['favorite_count'])
+        df['followers_count'] = pd.to_numeric(df['followers_count'])
+        df['friends_count'] = pd.to_numeric(df['friends_count'])
 
-        
         return df
-    
-    def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
+
+    def remove_non_english_tweets(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         remove non english tweets from lang
         """
-        def is_english():
-            nltk.download('words')
-            words = set(nltk.corpus.words.words())
-            sentence=" ".join(w for w in nltk.wordpunct_tokenize(sent) if w.lower() in words or not w.isalpha())
-        df = df["text"].apply(is_english())
-        
+        df = df[df['lang'] == 'en']
+        df.reset_index()
         return df
+
+    cleaned_df = Clean_Tweets(tweet_df)
+    #cleaned_df.to_csv("./data/cleaned_covid19.csv")
--- extract_dataframe.py
+++ extract_dataframe.py
@@ -37,64 +37,129 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self) -> list:
-
-        statuses_count
+        statuses_count = []
+        for i in self.tweets_list:
+            statuses_count.append(i['user']['statuses_count'])
+        return statuses_count
 
     def find_full_text(self) -> list:
         text = []
+        for x in range(len(self.tweets_list)):
+            text_data = self.tweets_list[x]['text']
+            text.append(text_data)
+        return text
 
     def find_sentiments(self, text) -> list:
-        text=self.text
-        polarity = TextBlob(text).sentiment.polarity
-        self.subjectivity = TextBlob(text).sentimentsubjectivity
+        polarity = []
+        subjectivity = []
 
-        return polarity, self.subjectivity
+        for each in text:
+            if (each):
+                result = TextBlob(str(each)).sentiment
+                polarity.append(result.polarity)
+                subjectivity.append(result.subjectivity)
+
+        return polarity, subjectivity
 
     def find_created_time(self) -> list:
-
+        created_at = []
+        for x in range(len(self.tweets_list)):
+            text = self.tweets_list[x]['created_at']
+            created_at.append(text)
         return created_at
 
     def find_source(self) -> list:
         source = []
-
+        for x in range(len(self.tweets_list)):
+            text = self.tweets_list[x]['source']
+            source.append(text)
         return source
 
     def find_screen_name(self) -> list:
         screen_name = []
+        for i in self.tweets_list:
+            screen_name.append(i['user']['screen_name'])
+        return screen_name
 
     def find_followers_count(self) -> list:
-        followers_count = []
+        friends_count = []
+        for i in self.tweets_list:
+            friends_count.append(i['user']['friends_count'])
+        return friends_count
 
     def find_friends_count(self) -> list:
         friends_count = []
+        for i in self.tweets_list:
+            friends_count.append(i['user']['friends_count'])
+        return friends_count
 
     def is_sensitive(self) -> list:
-        try:
-            is_sensitive = [x['possibly_sensitive'] for x in self.tweets_list]
-        except KeyError:
-            is_sensitive = None
-
-        return is_sensitive
+        isSensitive = []
+        for sensitive in self.tweets_list:
+            try:
+                is_sensitive = sensitive['possibly_sensitive']
+            except KeyError:
+                is_sensitive = None
+            isSensitive += [is_sensitive]
+        return isSensitive
 
     def find_favourite_count(self) -> list:
-        favourite_count
+        favorite_count = []
+        for x in range(len(self.tweets_list)):
+            text = self.tweets_list[x]['favorite_count']
+            try:
+                favorite_count.append(text)
+            except:
+                favorite_count.append('None')
+        return favorite_count
 
     def find_retweet_count(self) -> list:
         retweet_count = []
+        for x in range(len(self.tweets_list)):
+            text = self.tweets_list[x]['retweet_count']
+            try:
+                retweet_count.append(text)
+            except:
+                retweet_count.append('None')
+        return retweet_count
 
     def find_hashtags(self) -> list:
         hashtags = []
+        for i in self.tweets_list:
+            hashtags.append(i['entities']['hashtags'])
+        return hashtags
 
     def find_mentions(self) -> list:
         mentions = []
+        for i in self.tweets_list:
+            try:
+                mentions.append(i['entities']['user_mentions'])
+
+            except TypeError:
+                mentions = ''
+
+        return mentions
 
     def find_location(self) -> list:
-        try:
-            location = self.tweets_list['user']['location']
-        except TypeError:
-            location = ''
+        location = []
+        for i in self.tweets_list:
+            try:
+                location.append(i['user']['location'])
+
+            except TypeError:
+                location = ''
 
         return location
+
+    def find_lang(self) -> list:
+        lang = []
+        for x in range(len(self.tweets_list)):
+            text = self.tweets_list[x]['lang']
+            try:
+                lang.append(text)
+            except:
+                lang.append('None')
+        return lang
 
     def get_tweet_df(self, save=False) -> pd.DataFrame:
         """required column to be generated you should be creative and add more features"""
@@ -138,5 +203,5 @@ if __name__ == "__main__":
     _, tweet_list = read_json("../covid19.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df()
-
+    #tweet_df.to_csv("./data/covid19.csv")
     # use all defined functions to generate a dataframe with the specified columns above
